7a762c06962455300e0a7d961b8e9a74c58ab1af
Ben Armstrong <synrg@debian.org>
2019-08-28 13:35:19 -0300

    Bump version# up to 0.1.5.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="quaggagriff",
-    version="0.1.4",
+    version="0.1.5",
     author="Ben Armstrong",
     author_email="synrg@debian.org",
     description="A Discord bot for naturalists",
