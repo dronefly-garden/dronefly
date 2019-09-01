ac18d0220959be4ab301d05f2b524352e167e9c1
Ben Armstrong <synrg@debian.org>
2019-09-01 15:37:45 -0300

    Refactor to reduce complexity.

--- cogs/ebirdcog/ebirdcog.py
+++ cogs/ebirdcog/ebirdcog.py
@@ -18,23 +18,10 @@ class EBirdCog(commands.Cog):
 
     @commands.command()
     async def hybrids(self, ctx):
-        """From eBird, get recent hybrid sightings for a region and report them."""
-        ebird_key = await self.bot.db.api_tokens.get_raw("ebird", default={"api_key": None})
-        if ebird_key["api_key"] is None:
-            return await ctx.send("The eBird API key has not been set.")
-        region = await self.config.region()
-        days = await self.config.days()
+        """Report recent hybrid observations."""
         datetime_format = await self.config.datetime_format()
 
-        # Docs at: https://github.com/ProjectBabbler/ebird-api
-        records = get_observations(
-            ebird_key["api_key"],
-            region,
-            back=days,
-            category="hybrid",
-            detail="simple",
-            provisional=True,
-        )
+        records = await self.get_hybrid_observations(ctx)
         message = []
         for record in records:
             sciname = record['sciName']
@@ -51,3 +38,25 @@ class EBirdCog(commands.Cog):
             )
             message.append(line)
         await ctx.send("\n".join(message))
+
+    async def get_hybrid_observations(self, ctx):
+        """Get recent hybrid observations."""
+        ebird_key = await self.get_api_key(ctx)
+        region = await self.config.region()
+        days = await self.config.days()
+        # Docs at: https://github.com/ProjectBabbler/ebird-api
+        return get_observations(
+            ebird_key["api_key"],
+            region,
+            back=days,
+            category="hybrid",
+            detail="simple",
+            provisional=True,
+        )
+
+    async def get_api_key(self, ctx):
+        """Get API key."""
+        key = await self.bot.db.api_tokens.get_raw("ebird", default={"api_key": None})
+        if key["api_key"] is None:
+            return await ctx.send("The eBird API key has not been set.")
+        return key
