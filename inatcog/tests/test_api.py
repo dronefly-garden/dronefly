5310cc04fa7b4e7c975f1a604428dc53fcc981ae
eward <eward@sunbelt-medical.com>
2019-10-15 14:32:04 -0700

    API_REQUESTS_PATCH

--- inatcog/tests/test_api.py
+++ inatcog/tests/test_api.py
@@ -4,13 +4,15 @@ from unittest.mock import patch
 
 from inatcog import api
 
+API_REQUESTS_PATCH = patch("inatcog.api.requests.get")
+
 
 class TestAPI(unittest.TestCase):
     def test_get_taxa_by_id(self):
         """Test get_taxa by id."""
         expected_result = {"results": [{"name": "Animalia"}]}
 
-        with patch("inatcog.api.requests.get") as mock_get:
+        with API_REQUESTS_PATCH as mock_get:
             mock_get.return_value.json.return_value = expected_result
             self.assertEqual(api.get_taxa(1)["results"][0]["name"], "Animalia")
 
@@ -18,7 +20,7 @@ class TestAPI(unittest.TestCase):
         """Test get_taxa with query terms."""
         expected_result = {"results": [{"name": "Animalia"}]}
 
-        with patch("inatcog.api.requests.get") as mock_get:
+        with API_REQUESTS_PATCH as mock_get:
             mock_get.return_value.json.return_value = expected_result
             self.assertEqual(
                 api.get_taxa(q="animals")["results"][0]["name"], "Animalia"
@@ -30,7 +32,7 @@ class TestAPI(unittest.TestCase):
             "total_bounds": {"swlat": 1, "swlng": 2, "nelat": 3, "nelng": 4}
         }
 
-        with patch("inatcog.api.requests.get") as mock_get:
+        with API_REQUESTS_PATCH as mock_get:
             mock_get.return_value.json.return_value = expected_result_1
             self.assertIsNone(api.get_observation_bounds([]))
             self.assertIsNone(api.get_observation_bounds(["1"]))
