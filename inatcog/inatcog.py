f44a9925d18d60c5600085497fd3a309bb50d2e2
Ben Armstrong <synrg@debian.org>
2020-04-18 18:36:21 -0300

    Make obs reactions consistent with taxon.

--- inatcog/inatcog.py
+++ inatcog/inatcog.py
@@ -583,13 +583,7 @@ class INatCog(Listeners, commands.Cog, name="iNat", metaclass=CompositeMetaClass
         try:
             filtered_taxon = await self.taxa_query.query_taxon(ctx, query)
             msg = await ctx.send(embed=await self.make_obs_counts_embed(filtered_taxon))
-            group_by = filtered_taxon.group_by
-            place = filtered_taxon.place
-            user = filtered_taxon.user
-            if group_by == "place" or (user and not place):
-                start_adding_reactions(msg, ["#️⃣", "📝"])
-            if group_by == "user" or (place and not user):
-                start_adding_reactions(msg, ["🏠", "📍"])
+            start_adding_reactions(msg, ["#️⃣", "📝", "🏠", "📍"])
         except ParseException:
             await ctx.send(embed=sorry())
             return
