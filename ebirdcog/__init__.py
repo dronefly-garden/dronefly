892a5a22534a2ca10158a88cfadfd1737cb3d53d
Ben Armstrong <synrg@debian.org>
2021-05-07 04:44:14 -0300

    Add end user data statements for both cogs.

--- ebirdcog/__init__.py
+++ ebirdcog/__init__.py
@@ -1,5 +1,9 @@
 """EbirdCog init."""
+from redbot.core.utils import get_end_user_data_statement
+
 from .ebirdcog import EBirdCog
+
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 
 def setup(bot):
