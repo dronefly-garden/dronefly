25d802144cb0a559c02ce39b76da7b95e4f43470
Ben Armstrong <synrg@debian.org>
2021-05-09 15:54:19 -0300

    Handle wrap from uneven len last page to first.

--- inatcog/commands/search.py
+++ inatcog/commands/search.py
@@ -7,7 +7,7 @@ import urllib.parse
 
 from redbot.core import checks, commands
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
-from inatcog.common import grouper
+from inatcog.common import grouper, LOG
 from inatcog.converters import NaturalCompoundQueryConverter
 from inatcog.places import PAT_PLACE_LINK
 from inatcog.projects import PAT_PROJECT_LINK
@@ -143,7 +143,16 @@ class CommandsSearch(INatEmbeds, MixinMeta):
             ctx, pages, controls, message, page, timeout, reaction
         ):  # pylint: disable=too-many-arguments
             was_selected = selected_index[0]
-            if was_selected == per_embed_page - 1:
+            LOG.info(
+                "page=%d len(results)=%d len(pages)=%d", page, len(results), len(pages)
+            )
+            page_len = (
+                per_embed_page
+                if (page + 1) < len(pages)
+                else len(results) - ((len(pages) - 1) * per_embed_page)
+            )
+            LOG.info("page_len=%d", page_len)
+            if was_selected == page_len - 1:
                 next_reaction = next_page_reaction
             else:
                 selected_index[0] += 1
