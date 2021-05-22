bd0462ba0140aa86093331ff210c00b5d97b847b
Ben Armstrong <synrg@debian.org>
2021-05-22 06:53:53 -0300

    Tidy up INatQuery with helpers.

--- inatcog/query.py
+++ inatcog/query.py
@@ -1,6 +1,6 @@
 """Module to query iNat."""
 import re
-from redbot.core.commands import BadArgument
+from redbot.core.commands import BadArgument, Context
 from .common import DEQUOTE
 from .controlled_terms import ControlledTerm, match_controlled_term
 from .converters import MemberConverter
@@ -40,92 +40,86 @@ VALID_OBS_OPTS = [
 ]
 
 
+def _get_options(query_options: list):
+    options = {}
+    # Accept a limited selection of options:
+    # - all of these to date apply only to observations, though others could
+    #   be added later
+    # - all options and values are lowercased
+    for (key, *val) in map(lambda opt: opt.lower().split("="), query_options):
+        val = val[0] if val else "true"
+        # - conservatively, only alphanumeric, comma, dash or
+        #   underscore characters accepted in values so far
+        # - TODO: proper validation per field type
+        if key in VALID_OBS_OPTS and re.match(r"^[a-z0-9,_-]*$", val):
+            options[key] = val
+    return options
+
+
 class INatQuery:
     """Query iNat for all requested entities."""
 
     def __init__(self, cog):
         self.cog = cog
 
-    async def get(self, ctx, query: Query, scientific_name=False, locale=None):
+    async def _get_user(self, ctx: Context, user: str):
+        try:
+            who = await MemberConverter.convert(ctx, re.sub(DEQUOTE, r"\1", user))
+        except BadArgument as err:
+            raise LookupError(str(err)) from err
+        user = await self.cog.user_table.get_user(who.member)
+        return user
+
+    async def _get_controlled_term(self, query_term: str, query_term_value: str):
+        controlled_terms_dict = await self.cog.api.get_controlled_terms()
+        controlled_terms = [
+            ControlledTerm.from_dict(term, infer_missing=True)
+            for term in controlled_terms_dict["results"]
+        ]
+        controlled_term = match_controlled_term(
+            controlled_terms, query_term, query_term_value
+        )
+        return controlled_term
+
+    async def get(self, ctx: Context, query: Query, scientific_name=False, locale=None):
         """Get all requested iNat entities."""
-        taxon = None
-        place = None
-        user = None
-        unobserved_by = None
-        id_by = None
-        project = None
-        controlled_term = None
-        options = {}
+        args = {}
+
         preferred_place_id = await self.cog.get_home(ctx)
-        if query.project:
-            project = await self.cog.project_table.get_project(ctx.guild, query.project)
-        if query.place:
-            place = await self.cog.place_table.get_place(
-                ctx.guild, query.place, ctx.author
-            )
-        if place:
-            preferred_place_id = place.place_id
-        if query.main:
-            taxon = await self.cog.taxon_query.maybe_match_taxon_compound(
+        args["project"] = (
+            await self.cog.project_table.get_project(ctx.guild, query.project)
+            if query.project
+            else None
+        )
+        args["place"] = (
+            await self.cog.place_table.get_place(ctx.guild, query.place, ctx.author)
+            if query.place
+            else None
+        )
+        if args["place"]:
+            preferred_place_id = args["place"].place_id
+        args["taxon"] = (
+            await self.cog.taxon_query.maybe_match_taxon_compound(
                 query,
                 preferred_place_id=preferred_place_id,
                 scientific_name=scientific_name,
                 locale=locale,
             )
-        if query.user:
-            try:
-                who = await MemberConverter.convert(
-                    ctx, re.sub(DEQUOTE, r"\1", query.user)
-                )
-            except BadArgument as err:
-                raise LookupError(str(err)) from err
-            user = await self.cog.user_table.get_user(who.member)
-        if query.unobserved_by:
-            try:
-                who = await MemberConverter.convert(
-                    ctx, re.sub(DEQUOTE, r"\1", query.unobserved_by)
-                )
-            except BadArgument as err:
-                raise LookupError(str(err)) from err
-            unobserved_by = await self.cog.user_table.get_user(who.member)
-        if query.id_by:
-            try:
-                who = await MemberConverter.convert(
-                    ctx, re.sub(DEQUOTE, r"\1", query.id_by)
-                )
-            except BadArgument as err:
-                raise LookupError(str(err)) from err
-            id_by = await self.cog.user_table.get_user(who.member)
-        if query.controlled_term:
-            (query_term, query_term_value) = query.controlled_term
-            controlled_terms_dict = await self.cog.api.get_controlled_terms()
-            controlled_terms = [
-                ControlledTerm.from_dict(term, infer_missing=True)
-                for term in controlled_terms_dict["results"]
-            ]
-            controlled_term = match_controlled_term(
-                controlled_terms, query_term, query_term_value
-            )
-        if query.options:
-            # Accept a limited selection of options:
-            # - all of these to date apply only to observations, though others could
-            #   be added later
-            # - all options and values are lowercased
-            for (key, *val) in map(lambda opt: opt.lower().split("="), query.options):
-                val = val[0] if val else "true"
-                # - conservatively, only alphanumeric, comma, dash or
-                #   underscore characters accepted in values so far
-                # - TODO: proper validation per field type
-                if key in VALID_OBS_OPTS and re.match(r"^[a-z0-9,_-]*$", val):
-                    options[key] = val
-
-        return QueryResponse(
-            taxon=taxon,
-            user=user,
-            place=place,
-            unobserved_by=unobserved_by,
-            id_by=id_by,
-            project=project,
-            options=options,
-            controlled_term=controlled_term,
+            if query.main
+            else None
         )
+        args["user"] = await self._get_user(ctx, query.user) if query.user else None
+        args["unobserved_by"] = (
+            await self._get_user(ctx, query.unobserved_by)
+            if query.unobserved_by
+            else None
+        )
+        args["id_by"] = await self._get_user(ctx, query.id_by) if query.id_by else None
+        args["controlled_term"] = (
+            await self._get_controlled_term(ctx, *query.controlled_term)
+            if query.controlled_term
+            else None
+        )
+        args["options"] = _get_options(query.options) if query.options else None
+
+        return QueryResponse(**args)
