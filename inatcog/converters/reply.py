8fffc192444bd41ceed2867a849a9ae56d4567f8
Ben Armstrong <synrg@debian.org>
2021-05-25 16:43:20 -0300

    Fix the fix. Argh.

--- inatcog/converters/reply.py
+++ inatcog/converters/reply.py
@@ -12,12 +12,13 @@ class TaxonReplyConverter:
     async def convert(cls, ctx: Context, argument: str = ""):
         """Default to taxon from replied to bot message."""
         ref = ctx.message.reference
-        msg = ref.cached_message or await ctx.channel.fetch_message(ref.message_id)
-        if msg and msg.embeds:
-            inat_embed = INatEmbed.from_discord_embed(msg.embeds[0])
-            taxon_id = inat_embed.taxon_id()
-            if taxon_id:
-                argument += f" of {taxon_id}"
+        if ref:
+            msg = ref.cached_message or await ctx.channel.fetch_message(ref.message_id)
+            if msg and msg.embeds:
+                inat_embed = INatEmbed.from_discord_embed(msg.embeds[0])
+                taxon_id = inat_embed.taxon_id()
+                if taxon_id:
+                    argument += f" of {taxon_id}"
         if not argument:
             raise BadArgument()
         return await NaturalQueryConverter.convert(ctx, argument)
