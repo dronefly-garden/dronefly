ed8fdcda0f97f49c1a2d553303c2a33ad14c3dca
Ben Armstrong <synrg@debian.org>
2021-06-20 10:47:50 -0300

    core: Query tests for taxon properties.

--- inatcog/core/tests/query_test.py
+++ inatcog/core/tests/query_test.py
@@ -1,1 +1,34 @@
-# TODO: tests are needed before going any further.
+"""Tests for query module."""
+
+from ..query.query import Query, TaxonQuery
+
+
+# pylint: disable=missing-class-docstring disable=no-self-use disable=missing-function-docstring
+class TestQuery:
+    def test_query_id(self):
+        query = Query(main=TaxonQuery(taxon_id=1))
+        assert str(query) == "1"
+
+    def test_query_terms(self):
+        query = Query(main=TaxonQuery(terms=["song", "sparrow"]))
+        assert str(query) == "song sparrow"
+
+    def test_query_phrase(self):
+        query = Query(
+            main=TaxonQuery(terms=["song", "sparrow"], phrases=[["song", "sparrow"]])
+        )
+        assert str(query) == "song sparrow"
+
+    def test_query_ranks(self):
+        query = Query(main=TaxonQuery(terms=["song", "sparrow"], ranks=["sp", "ssp"]))
+        assert str(query) == "song sparrow sp ssp"
+
+    def test_query_code(self):
+        query = Query(main=TaxonQuery(code="WTSP"))
+        assert str(query) == "WTSP"
+
+    def test_query_ancestor(self):
+        query = Query(
+            main=TaxonQuery(terms=["prunella"]), ancestor=TaxonQuery(terms=["animals"])
+        )
+        assert str(query) == "prunella in animals"
