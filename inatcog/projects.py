2fbc72e6fc93855c6beb74f39a15b3073a205bdf
Ben Armstrong <synrg@debian.org>
2022-04-17 08:04:59 -0300

    Fix broken definition of UserProject.

--- inatcog/projects.py
+++ inatcog/projects.py
@@ -16,30 +16,47 @@ class CollectionProjectRule(DataClassJsonMixin):
 
 
 @dataclass
+class CollectionProjectField(DataClassJsonMixin):
+    """A collection project field."""
+
+    field: str
+    value: Union[bool, str, int, List[int]]
+
+
+@dataclass
 class UserProject(DataClassJsonMixin):
     """A collection project for observations by specific users.
 
-    There are two basic kinds of UserProject. Both are collection projects,
-    and both are "Members only" with the following differences:
+    This class handles projects that include observations by members, with membership
+    being managed either solely by the user, soley by the admins, or a combination
+    of both:
 
-    1. prefers_user_trust projects are "Members only" with an open membership.
-        i.e. any user can join and become a member, and only their observations
-        are included.
-    2. "observed_by_user?" projects are "Members only" with a closed membership.
-        i.e. users added to the project by the project admins, and only their
-        observations will be included.
+    1. Open membership, users join on the web:
+       - members_only: True
+       - do not set any "observed_by_user?" rules
+    2. Closed membership, admins edit the project to join users:
+       - members_only: False
+       - set at least one "observed_by_user?" rule
+    3. Closed membership, users join on web and admins edit project to "approve" join:
+       - members_only: True
+       - set at least one "observed_by_user?" rule
+       - a user's observations are included when both the user joins *and* the
+         admin "approves" the join by adding a rule for them
     """
 
     project_id: int = field(metadata=config(field_name="id"))
     title: str
     user_ids: List[int]
-    prefers_user_trust: bool
+    search_parameters: List[CollectionProjectField]
     project_observation_rules: List[CollectionProjectRule]
     project_type: str
 
     def __post_init__(self):
         if self.project_type != "collection":
             raise TypeError
+
+    def members_only(self):
+        return next(iter([param.value for param in self.search_parameters if param.field == "members_only"]), False)
 
     def observed_by_ids(self):
         """Valid observer user ids for the project.
@@ -50,25 +67,43 @@ class UserProject(DataClassJsonMixin):
         - for closed membership projects, exclude any user that has both
           an included rule (observed_by_user?) and an excluded rule
           (not_observed_by_user?)
-        - for open membership projects, ignore any included user rules
-          and only obey excluded user rules
         """
         exclude_user_ids = [
             rule.operand_id
             for rule in self.project_observation_rules
             if rule.operator == "not_observed_by_user?"
         ]
-        if self.prefers_user_trust:
-            include_user_ids = [
-                user_id for user_id in self.user_ids if user_id not in exclude_user_ids
-            ]
+        include_user_rules = [
+            rule
+            for rule in self.project_observation_rules
+            if rule.operator == "observed_by_user?"
+            and rule.operand_id not in exclude_user_ids
+        ]
+
+        if self.members_only:
+            if include_user_rules:
+                # i.e. case 3, Closed (user joins & admin approves the join)
+                include_user_ids = [
+                    rule.operand_id for rule in include_user_rules if rule.operand_id in self.user_ids
+                ]
+            else:
+                # i.e. case 1, Open (user joins)
+                include_user_ids = [
+                    user_id for user_id in self.user_ids if user_id not in exclude_user_ids
+                ]
         else:
-            include_user_ids = [
-                rule.operand_id
-                for rule in self.project_observation_rules
-                if rule.operator == "observed_by_user?"
-                and rule.operand_id not in exclude_user_ids
-            ]
+            if include_user_rules:
+                # i.e. case 2, Closed (admin joins the user)
+                include_user_ids = [
+                    rule.operand_id for rule in include_user_rules
+                ]
+            else:
+                # i.e. fallback - project membership is undefined, so is empty
+                # - as in case 2 but admins haven't joined anyone yet
+                # - note: exclusions are irrelevant; an empty membership minus
+                #   some specific users is still "empty"
+                include_user_ids = []
+
         return include_user_ids
 
 
