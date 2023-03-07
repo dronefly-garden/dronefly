932e444ab0ba053ecaba599540ddb331e435e12a
Ben Armstrong <synrg@debian.org>
2023-03-07 19:16:08 -0400

    Support dpy2.

--- ebirdcog/__init__.py
+++ ebirdcog/__init__.py
@@ -1,4 +1,5 @@
 """EbirdCog init."""
+from redbot.core.bot import Red
 from redbot.core.utils import get_end_user_data_statement
 
 from .ebirdcog import EBirdCog
@@ -6,6 +7,9 @@ from .ebirdcog import EBirdCog
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 
-def setup(bot):
-    """Add cog to bot."""
-    bot.add_cog(EBirdCog(bot))
+async def setup(bot: Red) -> None:
+    """Load eBird cog."""
+    cog = EBirdCog(bot)
+    r = bot.add_cog(cog)
+    if r is not None:
+        await r
