4c5e94d6c8a2dff7a4a64d2aaf6eee3bd4b45a85
Ben Armstrong <synrg@debian.org>
2023-06-18 10:38:52 -0300

    Move basic parts of get query params to core.

--- inatcog/query.py
+++ inatcog/query.py
@@ -1,55 +1,18 @@
 """Module to query iNat."""
-import datetime as dt
 import re
 
-from dronefly.core.parsers.constants import VALID_OBS_OPTS
-from dronefly.core.query.query import DateSelector, Query, QueryResponse, TaxonQuery
+from dronefly.core.query.query import (
+    get_base_query_args,
+    has_value,
+    Query,
+    QueryResponse,
+)
 from dronefly.core.models.controlled_terms import match_controlled_term
 from pyinaturalist.models import ControlledTerm, User
 from redbot.core.commands import BadArgument, Context
 
 from .common import DEQUOTE
 from .converters.base import MemberConverter
-
-
-def _get_options(query_options: list):
-    options = {}
-    # Accept a limited selection of options:
-    # - all of these to date apply only to observations, though others could
-    #   be added later
-    # - all options and values are lowercased
-    for (key, *val) in map(lambda opt: opt.lower().split("="), query_options):
-        val = val[0] if val else "true"
-        # - conservatively, only alphanumeric, comma, dash or
-        #   underscore characters accepted in values so far
-        # - TODO: proper validation per field type
-        if key in VALID_OBS_OPTS and re.match(r"^[a-z0-9,_-]*$", val):
-            options[key] = val
-    return options
-
-
-def has_value(arg):
-    """Return true if arg is present and is not the `any` special keyword.
-
-    Use `any` in a query where a prior non-empty clause is present,
-    and that will negate that clause.
-    """
-    if not arg:
-        return False
-    if isinstance(arg, list):
-        return arg[0] and arg[0].lower() != "any"
-    elif isinstance(arg, TaxonQuery):
-        return (
-            (arg.terms and arg.terms[0].lower() != "any")
-            or arg.code
-            or arg.phrases
-            or arg.ranks
-            or arg.taxon_id
-        )
-    elif isinstance(arg, dt.datetime):
-        return True
-    else:
-        return arg.lower() != "any"
 
 
 class INatQuery:
@@ -101,7 +64,7 @@ class INatQuery:
         self, ctx: Context, query: Query, scientific_name=False, locale=None, **kwargs
     ):
         """Get all requested iNat entities."""
-        args = {}
+        args = get_base_query_args(query)
 
         args["project"] = (
             await self.cog.project_table.get_project(ctx.guild, query.project)
@@ -149,18 +112,4 @@ class INatQuery:
             if has_value(query.controlled_term)
             else None
         )
-        args["options"] = (
-            _get_options(query.options) if has_value(query.options) else None
-        )
-        _observed = {}
-        _observed["on"] = query.obs_on if has_value(query.obs_on) else None
-        _observed["d1"] = query.obs_d1 if has_value(query.obs_d1) else None
-        _observed["d2"] = query.obs_d2 if has_value(query.obs_d2) else None
-        args["observed"] = DateSelector(**_observed)
-        _added = {}
-        _added["on"] = query.added_on if has_value(query.added_on) else None
-        _added["d1"] = query.added_d1 if has_value(query.added_d1) else None
-        _added["d2"] = query.added_d2 if has_value(query.added_d2) else None
-        args["added"] = DateSelector(**_added)
-
         return QueryResponse(**args)
