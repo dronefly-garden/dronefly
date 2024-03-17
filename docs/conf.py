013aabf475d5b4247983c2d28c578e9bd619296e
Ben Armstrong <synrg@debian.org>
2024-03-17 11:15:18 -0300

    sphinx/rtd: Switch from PDJ to Material theme.

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,6 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.doctest",
     "sphinx.ext.imgconverter",
-    "sphinx_pdj_theme",
     # "sphinxcontrib_trio",
 ]
 
@@ -55,7 +54,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_pdj_theme"
+html_theme = "sphinx_material"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
