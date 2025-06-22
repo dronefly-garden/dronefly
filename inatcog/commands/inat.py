a4cc215bc90f5b75797311063f047d713795ee45
Ben Armstrong <synrg@debian.org>
2025-06-22 09:26:37 -0300

    Help for advanced `hour` option.

--- inatcog/commands/inat.py
+++ inatcog/commands/inat.py
@@ -340,7 +340,7 @@ class CommandsInat(INatEmbeds, MixinMeta):
         Boolean options without a parameter default to `=true`, e.g. `,tab my opt verifiable` means `,tab my opt verifiable=true`. Other values can be `=false` or `=any`.
 
         **Other options:**
-        `csi` `day` `month` `year` `hrank` `lrank` `id` `not_id` `quality_grade` `order` `order_by` `page` `rank` `iconic_taxa` `taxon_ids` `without_taxon_id` `geoprivacy` `taxon_geoprivacy` `q` `search_on`
+        `csi` `hour` `day` `month` `year` `hrank` `lrank` `id` `not_id` `quality_grade` `order` `order_by` `page` `rank` `iconic_taxa` `taxon_ids` `without_taxon_id` `geoprivacy` `taxon_geoprivacy` `q` `search_on`
 
         See the [get observations API documentation](https://api.inaturalist.org/v1/docs/#!/Observations/get_observations) for detailed descriptions of these options and what parameter values are allowed. Not all options make sense for all queries/commands.
         """  # noqa: E501
