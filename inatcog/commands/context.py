910a01276dd4e9ef211f045f000fd605a0dc81f7
Ben Armstrong <synrg@debian.org>
2026-09-06 17:07:09 -0300

    Show taxon: dig the taxon out of an observation if found instead of taxon

--- inatcog/commands/context.py
+++ inatcog/commands/context.py
@@ -6,7 +6,7 @@ import discord
 
 from redbot.core import app_commands
 
-from dronefly.core.parsers.url import PAT_TAXON_LINK
+from dronefly.core.parsers.url import PAT_OBS_LINK, PAT_TAXON_LINK
 
 from ..constants import COG_NAME
 from ..partials import PartialContext
@@ -38,6 +38,19 @@ async def show_taxon(interaction: discord.Interaction, message: discord.Message)
         mat_taxon = re.search(PAT_TAXON_LINK, message.content)
         if mat_taxon:
             taxon_id = mat_taxon["taxon_id"]
+        else:
+            obs_id = None
+            mat_obs = re.search(PAT_OBS_LINK, message.content)
+            if mat_obs:
+                obs_id = mat_obs["obs_id"]
+            if obs_id:
+                obs = await anext(
+                    aiter(cog.inat_client.observations.from_ids(int(obs_id))), None
+                )
+                if obs:
+                    taxon = obs.taxon
+                    if taxon:
+                        taxon_id = taxon.id
     if taxon_id:
         taxon_command = bot.get_command("taxon")
         await taxon_command(ctx, query=str(taxon_id))
